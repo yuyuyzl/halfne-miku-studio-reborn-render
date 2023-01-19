ecbf06b66fda0d68cf008a84229ebbe529fee696
yuyuyzl <yuzhongliang221@gmail.com>
2023-01-19 23:38:13 +0800

    release

--- 01.预览输出配置（拖放json到这里）.bat
+++ 01.预览输出配置（拖放json到这里）.bat
@@ -1,4 +1,5 @@
 cd /d %~dp0
+set path=%path%;%cd%\bin
 copy %1 src\in.json
 call npx remotion preview src/index.jsx
 pause
--- 02A.输出mp4（拖放json到这里）.bat
+++ 02A.输出mp4（拖放json到这里）.bat
@@ -1,5 +1,5 @@
 cd /d %~dp0
-set path=%path%;%cd%
+set path=%path%;%cd%\bin
 copy %1 src\in.json
 call npx remotion render src/index.jsx HelloWorld out/video.mp4
 pause
