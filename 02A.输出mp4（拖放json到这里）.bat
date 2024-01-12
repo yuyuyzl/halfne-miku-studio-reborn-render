d209990ca00225fa66b4941e456c79784fe4725d
yuyuyzl <yuzhongliang221@gmail.com>
2024-01-13 01:38:28 +0800

    support node > 17

--- 02A.输出mp4（拖放json到这里）.bat
+++ 02A.输出mp4（拖放json到这里）.bat
@@ -1,5 +1,7 @@
 cd /d %~dp0
 set path=%path%;%cd%\bin
+for /f "tokens=1 delims=v" %%a in ('node -v') do set version=%%a
+if %version% GTR 17 set NODE_OPTIONS=--openssl-legacy-provider
 copy %1 src\in.json
 call npx remotion render src/index.jsx HelloWorld out/video.mp4
 pause
