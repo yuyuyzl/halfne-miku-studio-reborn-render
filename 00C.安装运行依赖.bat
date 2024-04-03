b8850fa7ce94849ebef5a8a20a333809c895f905
yuyuyzl <yuzhongliang221@gmail.com>
2024-04-03 20:35:31 +0800

    bin bats

---
+++ 00C.安装运行依赖.bat
@@ -0,0 +1,19 @@
+cd /d %~dp0
+set path=%cd%\bin;%path%
+where node
+if %ERRORLEVEL% NEQ 0 (
+    echo bin/nodejs.exe not found.
+    echo Check and follow link in /bin folder.
+    pause
+)
+where ffmpeg
+if %ERRORLEVEL% NEQ 0 (
+    echo bin/ffmpeg.exe not found.
+    echo Check and follow link in /bin folder.
+    pause
+)
+set version=0
+for /f "tokens=1 delims=v" %%a in ('node -v') do set version=%%a
+if %version% GTR 17 set NODE_OPTIONS=--openssl-legacy-provider
+npm install
+pause
