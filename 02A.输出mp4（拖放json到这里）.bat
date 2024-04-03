973c13eb03a8d1b36d1efe5ca4e61cf9b071d5e7
yuyuyzl <yuzhongliang221@gmail.com>
2024-04-03 22:50:59 +0800

    bin bats

--- 02A.输出mp4（拖放json到这里）.bat
+++ 02A.输出mp4（拖放json到这里）.bat
@@ -3,5 +3,6 @@ set path=%cd%\bin;%path%
 for /f "tokens=1 delims=v" %%a in ('node -v') do set version=%%a
 if %version% GTR 17 set NODE_OPTIONS=--openssl-legacy-provider
 copy %1 src\in.json
+if not exist "out" mkdir out
 call npx remotion render src/index.jsx HelloWorld out/video.mp4
 pause
