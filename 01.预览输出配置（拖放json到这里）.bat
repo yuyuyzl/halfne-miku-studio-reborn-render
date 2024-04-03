16a9f39fb49f5502f1e87af53e4618cc3e066101
yuyuyzl <yuzhongliang221@gmail.com>
2024-04-03 19:05:49 +0800

    调整path顺序

--- 01.预览输出配置（拖放json到这里）.bat
+++ 01.预览输出配置（拖放json到这里）.bat
@@ -1,5 +1,5 @@
 cd /d %~dp0
-set path=%path%;%cd%\bin
+set path=%cd%\bin;%path%
 for /f "tokens=1 delims=v" %%a in ('node -v') do set version=%%a
 if %version% GTR 17 set NODE_OPTIONS=--openssl-legacy-provider
 copy %1 src\in.json
